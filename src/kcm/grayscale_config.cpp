427391817b356effc70331de37842cfdad136719
ugjka <ugis.germanis@gmail.com>
2018-08-28 20:39:38 +0300

    Update grayscale_config.cpp

--- src/kcm/grayscale_config.cpp
+++ src/kcm/grayscale_config.cpp
@@ -3,7 +3,7 @@
  This file is part of the KDE project.
 
 Copyright (C) 2007 Rivo Laks <rivolaks@hot.ee>
-Copyright (C) 2018 Ugis Germanis <ugis.germanis@gmail.com>
+Copyright (C) 2018 2017 ugjka <esesmu@protonmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
